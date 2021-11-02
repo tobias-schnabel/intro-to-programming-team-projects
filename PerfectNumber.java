d84dbd15260fa2d2b0b16a08be8f7ecb6198a4d7
ts <ctobiasschnabel@gmail.com>
2021-11-02 12:15:35 +0100

    started P-2-6

---
+++ PerfectNumber.java
@@ -0,0 +1,15 @@
+/* DayOfWeek.java
+Authors: Dohmen, RJH (i6250494)
+         Schnabel, CT (i6255807)*/
+
+import java.util.Scanner;
+
+/*A positive integer is called perfect, if the sum of its proper divisors is equal to the number
+(e.g. 6 = 1+2+3, 28 = 1 + 2 + 4 + 7 + 14). Write a method isPerfectNumber of return type boolean
+that has one int parameter n and that returns true if n is a perfect number. Write a program PerfectNumber
+that takes as input a positive integer parameter and then calls the above isPerfectNumber method with that
+parameter and outputs a corresponding message.
+ */
+
+public class PerfectNumber {
+}
