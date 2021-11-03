5479a5f51318ed073fd96affa9e54d965b295e03
ts <ctobiasschnabel@gmail.com>
2021-11-03 14:28:47 +0100

    started  G-2-2

---
+++ SumOfDigitNumbers.java
@@ -0,0 +1,21 @@
+
+/* DayOfWeek.java
+Authors: Dohmen, RJH (i6250494)
+         Schnabel, CT (i6255807)*/
+
+import java.util.Scanner;
+import java.lang.Math;
+
+/*A digit sum number is a positive integer, which contains a digit
+that is equal to the sum of all the other digits. There are no
+digit sum numbers which have one digit, and all digit sum numbers with two digits
+are multiples of 11. Some examples of digit sum numbers having three digits are
+352 (5 = 3+2), 312 (3 = 1+2) and 134 (4 = 1 + 3). Write a program SumOfDigitSumNumbers
+ that takes as input a positive integer value n and outputs the sum of all digit sum numbers
+  which have at most n digits. */
+    public class SumOfDigitNumbers {
+
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+        } //close main
+} //close class
