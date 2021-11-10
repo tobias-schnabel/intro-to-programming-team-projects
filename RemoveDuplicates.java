d77a5ab45a583890ee759903a5bd6174b1b95934
ts <ctobiasschnabel@gmail.com>
2021-11-10 17:24:58 +0100

    set up G-3-1 and G-3-2

---
+++ RemoveDuplicates.java
@@ -0,0 +1,29 @@
+/* RemoveDuplicates.java
+Authors: Dohmen, RJH (i6250494)
+         Schnabel, CT (i6255807)
+Write a method removeDuplicates that takes as input an array of
+integer values and that returns the same array, but with duplicate
+values being removed.
+Write a program RemoveDuplicates that tests your method.*/
+
+import java.util.*;
+import java.io.*;
+
+public class RemoveDuplicates {
+
+    public static void main (String[] args) {
+      //  System.out.println("Please enter an Array of integer values");
+    //to do: get array input: how? randomized? from file? -> ask Arman
+
+        int n = 0; //length of input array
+        int[] arrayIn = new int[n]
+        System.out.println("Original Array: " + arrayIn);
+        System.out.println("Array without duplicates: " + removeDuplicates(arrayIn));
+
+    } //close main
+
+    public static int[] removeDuplicates(int[] inArray) {
+
+        return null; //stub
+    } //close method
+}
