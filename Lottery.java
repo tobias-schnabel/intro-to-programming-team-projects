ec4aa341633b050797d915413433d957578d9e8f
ts <ctobiasschnabel@gmail.com>
2021-11-11 17:01:27 +0100

    finalized G-3-1 improved G-3-2

--- Lottery.java
+++ Lottery.java
@@ -97,21 +97,21 @@ public class Lottery {
         int n = array.length;
         int[] arrayNoDup = new int[n]; //create new, empty array with same length
         int counter =0; //counts non-dups in final array (see inner loop)
-        for (int i =0; i < n; i++) {
+        for (int k : array) {
             boolean isDup = false; //stores whether number is false for each i
-            for (int j =0; j < counter; j++){ //second running index
-                if (arrayNoDup[j] == array[i]){
+            for (int j = 0; j < counter; j++) { //second running index
+                if (arrayNoDup[j] == k) {
                     isDup = true; //number already contained in return array
+                    break;
                 }
             } //close inner loop
-            if (isDup ==false) { //if for all j, number only present once, it is assigned in output array
-                arrayNoDup[counter]=array[i];
+            if (!isDup) { //if for all j, number only present once, it is assigned in output array
+                arrayNoDup[counter] = k;
                 counter++; //ups counter to move to next index in output array
             }
         } //close outer loop
 
-        int[] arrayOut = Arrays.copyOf(arrayNoDup, counter); //creates return array as trimmed copy of output array
-        return arrayOut;
+        return Arrays.copyOf(arrayNoDup, counter);
     } //close remove duplicates method
 
 
