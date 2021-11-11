81ced18aa555d713f9d9a8d5b1728148be2f2c97
RDHMN <93044236+RDHMN@users.noreply.github.com>
2021-11-11 11:12:40 +0100

    intermediate G-3-1

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -7,22 +7,47 @@ values being removed.
 Write a program RemoveDuplicates that tests your method.*/
 
 import java.util.*;
-import java.io.*;
 
 public class RemoveDuplicates {
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
 
-    public static void main (String[] args) {
-      //  System.out.println("Please enter an Array of integer values");
-    //to do: get array input: how? randomized? from file? -> ask Arman
+        System.out.print("Enter the size of the array: ");
+            int n = in.nextInt();   //length of input array
+            int[] arrayIn = new int[n]; //init array
 
-        int n = 0; //length of input array
-        int[] arrayIn = new int[n]
-        System.out.println("Original Array: " + arrayIn);
-        System.out.println("Array without duplicates: " + removeDuplicates(arrayIn));
-
+        for(int i = 0; i < n; i++){ //constructs an array of lenght n by means of user input
+            System.out.printf("Enter array value %d: ", i+1);
+            arrayIn[i] = in.nextInt(); 
+        }
+        System.out.println("Original Array: " + Arrays.toString(arrayIn));
+        System.out.println("Array without duplicates: " + Arrays.toString(removeDuplicates(arrayIn, n)));
     } //close main
 
-    public static int[] removeDuplicates(int[] inArray) {
+    public static int[] removeDuplicates(int array[], int n) {
+
+        int arrayTemp[] = new int[n];
+        int j = 0;
+
+       /* for(int i = 0; i < n; i++){
+            for(j = i+1; j < n){
+                if (array[i]==array[j]){
+                }
+            }
+        } */
+
+/*
+        int j = 0;
+        for (int i = 0; i < n-1; i++){
+            if (array[i] != array[i+1]){
+                arrayTemp[j++] = array[i];
+            }
+        }
+        arrayTemp[j++] = array[n-1];
+        /*
+        for (int i = 0; i < j; i++){
+            arrayIn[i] = arrayTemp[i];
+        }*/
 
         return null; //stub
     } //close method
