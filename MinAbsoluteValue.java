e98bdf7210be1ce534629d98839cdf5878799a27
ts <ctobiasschnabel@gmail.com>
2021-11-15 17:06:52 +0100

    wokred on P-4-2

--- MinAbsoluteValue.java
+++ MinAbsoluteValue.java
@@ -18,9 +18,33 @@ import java.util.Arrays;
 
 public class MinAbsoluteValue {
 
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int n = in.nextInt();   //length of input array
+        int[] array = new int[n]; //init array
+        array = randomIntArray(n);
+            //runtime check
+            double startTime1 = System.currentTimeMillis();
+            int minValue1 = minAbsoluteValueLinear(array);
+            double endTime1 = System.currentTimeMillis();
+            double totalTime1 = endTime1 - startTime1;
+
+            double startTime2 = System.currentTimeMillis();
+            int minValue2 = minAbsoluteValueBinary(array);
+            double endTime2 = System.currentTimeMillis();
+            double totalTime2 = endTime2 - startTime2;
+
+        System.out.println("Algorithm 1 took " + (totalTime1) + " milliseconds.");
+        System.out.println("Algorithm 2 took " + (totalTime2) + " milliseconds.");
+        System.out.println("To verify: \n Random Array: " + Arrays.toString(array) + "\n Min Linear Method: " + minValue1 + "\n Min Binary Method: " +minValue2);
+
+
+    } //close main
 
     public static int minAbsoluteValueLinear(int[] array) {
-        int minValue = 0;
+        int minValue = array[0];
         for (int i = 0; i < array.length; i++) {
             if (Math.abs(array[i]) < Math.abs(minValue) ) {
                 minValue = array[i];
@@ -30,21 +54,32 @@ public class MinAbsoluteValue {
     } //close method
 
     public static int minAbsoluteValueBinary(int[] array) {
-        int minValue = 0;
+        int minValue = array[0];
         int low = 0;
         int high = array.length -1;
         while (low+1 < high ) {
-            int mid = (high + low) /2;
+            int mid = (high + low) / 2;
 
-            if (Math.abs(array[mid]) < Math.abs(minValue) ) {
+            if (Math.abs(array[mid]) < Math.abs(minValue)) {
                 minValue = array[mid];
-            } else if (Math.abs(array[mid]) == Math.abs(minValue)){
+            } else if (Math.abs(array[mid]) == Math.abs(minValue)) {
                 break;
             } else if (Math.abs(array[mid]) > Math.abs(minValue)) {
                 low = mid;
             }
+        }
 
         return minValue;
     } //close method
+
+        // returns an array of length n containing random double values
+        public static int[] randomIntArray(int n){
+            Random random = new Random();
+            int[] array = new int[n];
+            for(int i=0; i < n; i++){
+                array[i] = random.nextInt();
+            }
+            return array;
+        }
 
 } // closeclass
