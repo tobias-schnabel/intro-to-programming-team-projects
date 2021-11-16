d958f52c52f682cfd1d8035a683a5293f971bc62
ts <ctobiasschnabel@gmail.com>
2021-11-16 12:08:10 +0100

    set up G-4-2

---
+++ RunTimesKnapsack.java
@@ -0,0 +1,30 @@
+/*RunTimesKnapsack.java
+        Authors: Dohmen, RJH (i6250494)
+        Schnabel, CT (i6255807)
+        Write a program RunTimesKnapsack that implements
+        the algorithm that was presented in the lecture to solve the knapsack problem.
+
+        Experimentally analyse the runtime of the algorithm depending on the number of items
+        of the instance as well as on the volume bound.
+
+        Given are n items with p roﬁts p i and volumes v i as well as a kna p sack of volume V .
+
+Which items should be p acked in the kna p sack as to maximize their total p roﬁts, while still having
+ total volume at most V ?
+ */
+
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class RunTimesKnapsack {
+
+    public static void main(String[] args) {
+// to add in main:
+
+        //user input: # items, respective volumes, knapsack volume
+        //final output
+        //call method
+
+    } //close main
+} //close class
