b62aed384d5d1edbf733e66f579ee6aee4be1ab3
ts <ctobiasschnabel@gmail.com>
2021-11-18 12:51:59 +0100

    worked on G-4-2

--- RunTimesKnapsack.java
+++ RunTimesKnapsack.java
@@ -61,15 +61,38 @@ public class RunTimesKnapsack {
     public static int KnapsackOptimize(int[] profitArr, int[] volumeArr, int volumeBound){
         int n = profitArr.length; //number of items is same as # elements in profit array
         int[][] returnArray = new int[(n + 1)][(volumeBound + 1)]; //creates return array with dims [n+1] [V+1]
+        for (int i = 1; i <= n; i++) {
         for (int v = 0; v <= volumeBound; v++){
-            returnArray[0][v] = 0; }
-            for (int i = 1; i <= n; i++) {
-                for (int v = 1; v <= volumeBound; v++) {
-                    if (volumeArr[i] <= v){
-                        returnArray[i][v] = Math.max((profitArr[i-1] + returnArray[i-1][(v - volumeArr[i])]), returnArray[i-1][v]);
-                    } else returnArray[i][v] = returnArray[i-1][v];
-                }
+            if (i == 0 || v == 0) { //base case
+                returnArray[i][v] = 0;
+            } else if (volumeArr[i-1] <= volumeBound) {
+                returnArray[i][v] = Math.max((profitArr[i-1] + returnArray[i-1][(v - volumeArr[i-1])]), returnArray[i-1][v]);
             }
+            returnArray[0][v] = 0; }
+
+            } //close outer for
     return returnArray[n][volumeBound];
+
+//        int[][] matrix = new int[lengthArray+1][capacity+1];
+//
+//        for (int i = 0; i <= lengthArray; i++) {
+//            for (int v = 0; v <= capacity; v++) {
+//                if (i == 0 || v == 0){ //base case in recursive method.
+//                    matrix[i][v] = 0;
+//                }
+//                else if (volumeI[i-1] <= v){ //if the volume of the i-th item is less than V.
+//                    matrix[i][v] = Math.max(profitI[i-1] + matrix[i-1][v - volumeI[i-1]], matrix[i-1][v]);
+//                }
+//                //first part is if you decide to include the element in the knapsack.
+//                //Second part is if you do not decide to include the element in the knapSack.
+//                //Note that max method should still be defined.
+//
+//                else{
+//                    matrix[i][v] = matrix[i-1][v]; //if the volume of the i-th item is more than the V, then it cannot be included.
+//                }
+//            } //close inner loop
+//        } //close outer loop
+//
+//        return matrix[lengthArray][capacity];
     } //close method
 } //close class
