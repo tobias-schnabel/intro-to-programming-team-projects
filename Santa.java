33dadf0a4b8ebfde570e66f3d0c7c0db74235bb6
Tobias Schnabel <70516553+CTS98@users.noreply.github.com>
2021-12-01 10:43:09 +0100

    solved G-6-1 (though we never seem to get any ties)

--- Santa.java
+++ Santa.java
@@ -1,14 +1,13 @@
 /*RunTimesMatrixMulitplication.java
         Authors: Dohmen, RJH (i6250494)
         Schnabel, CT (i6255807)
-
  */
 
 public class Santa {
 
     public static void main(String[] args){
 
-        IntegerStack randomStack = new IntegerStack();
+        IntegerStack randomStack;
 
         //counter vars
         int santaWins = 0;
@@ -26,21 +25,21 @@ public class Santa {
            } else{ ties += 1;}
            } //close loop
         // print output
-        if (100000-santaWins-alabWins == ties) { //redundant validation
             System.out.println("The game was played 100.000 times, each time using a new randomly generated stack of presents.");
             System.out.println("Of those 100.000 times, Santa won " + santaWins + " times, Alabaster won " + alabWins + " times, and there were " + ties + " ties.");
-            ties += 1;
-        }
+
+
     } //close main
+
+
 
     //ﬁrst write a method that creates a random stack of 12 red and 12 blue presents
     public static IntegerStack randomStack (int blue,  int red){
        IntegerStack returnStack = new IntegerStack();
 
-       int n = 24;
-       int countBlue = blue; //red is 0 //12
+        int countBlue = blue; //red is 0 //12
        int countRed = red; //blue is 1 //12
-       double probability = 0.0; //initialize probability
+       double probability; //initialize probability
 
        while (returnStack.size() != 24){
            probability = ((double) countRed) / ((double) (countRed + countBlue));
@@ -55,56 +54,66 @@ public class Santa {
        } //close loop
 
         return returnStack;
-
     } //close method
+
+
 
     public static  int santaGame(IntegerStack inputStack) {
         //RED IS ZERO, BLUE IS 1
         int outcome = 0;
-        int dayCount = 0;
         int santaPresents = 0;
         int alabasterPresents = 0;
 
-            while (santaPresents != 12 || alabasterPresents != 12) {
-                double coinFlip = Math.random();
-
-
-                    while (inputStack != null && !inputStack.isEmpty()) {
-                        if (coinFlip <= 0.5) {
-                            int firstPresent1 = inputStack.pop();
-                            int secondPresent1 = inputStack.pop();
-                            if (firstPresent1 == 1 && secondPresent1 == 1) { //case: 2 blue presents
-                                inputStack.push(1);
-                            } else if (firstPresent1 == 0 || secondPresent1 == 0) { //cases: red and blue present & 2 red presents
-                                santaPresents++;
-                                inputStack.push(1);
-                            }
-                        } else if (coinFlip > 0.5) {//Alabaster's turn
-
-                            int firstPresent2 = inputStack.pop();
-                            int secondPresent2 = inputStack.pop();
-                            if (firstPresent2 == 1 && secondPresent2 == 1) {  //case: 2 blue presents
-                                alabasterPresents++;
-                                inputStack.push(0);
-                            } else if (firstPresent2 == 0 && secondPresent2 == 0) { //case: 2 red presents
-                                inputStack.push(1);
-                            } else {                      //case: presents of different color
-                                alabasterPresents++;
-                                inputStack.push(0);
-                            }
+        for (int dayCount = 0; dayCount  <= 24; dayCount++) {
+            double coinFlip = Math.random();
+                // Days 1 - 23
+                while (inputStack.size() > 1 && !inputStack.isEmpty()) {
+                    if (coinFlip <= 0.5) { // Santa's turn
+                        int firstPresent1 = inputStack.pop();
+                        int secondPresent1 = inputStack.pop();
+                        if (firstPresent1 == 1 && secondPresent1 == 1) { //case: 2 blue presents
+                            inputStack.push(1);
+                        } else if (firstPresent1 == 0 || secondPresent1 == 0) { //cases: red and blue present & 2 red presents
+                            santaPresents++;
+                            inputStack.push(1);
                         }
-                    } //close inner while loop
-                    } //close outer while loop
+                    } else if (coinFlip > 0.5) {//Alabaster's turn
 
-            if (santaPresents == 12 && alabasterPresents == 12) {
-                outcome = 0;
-            } else if (santaPresents == 12 && alabasterPresents != 12) {
+                        int firstPresent2 = inputStack.pop();
+                        int secondPresent2 = inputStack.pop();
+                        if (firstPresent2 == 1 && secondPresent2 == 1) {  //case: 2 blue presents
+                            alabasterPresents++;
+                            inputStack.push(0);
+                        } else if (firstPresent2 == 0 && secondPresent2 == 0) { //case: 2 red presents
+                            inputStack.push(1);
+                        } else {                      //case: presents of different color
+                            alabasterPresents++;
+                            inputStack.push(0);
+                        }
+                    }
+                } //close inner while loop
+            // Day 24 //Santa: red, Alab: blue
+            if (dayCount == 24 && !inputStack.isEmpty()) {
+                int lastPresent = inputStack.pop();
+                if (coinFlip <= 0.5) { // Santa's turn
+                     if (lastPresent == 0){
+                            santaPresents += 1;
+                     }
+                    } else { // ALabs's turn
+                    if (lastPresent == 0){
+                        alabasterPresents += 1;
+                    }
+                }
+            }
+        } //close outer for loop
+            // outcome 0  (tie) is initializer value, only changed here if we have a winner
+            if (santaPresents > alabasterPresents) {
                 outcome = 1;
-            } else if (alabasterPresents == 12 && santaPresents != 12) {
+            } else if (alabasterPresents > santaPresents) {
                 outcome = -1;
             }
 
-        return outcome;
+    return outcome;
     } //close method
 
 } //close class
